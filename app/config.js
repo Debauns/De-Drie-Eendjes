c212ac49528a419bbdb06eb66bc08574e286fb89
Debauns <laurensvanwesting@gmail.com>
2025-04-10 20:56:11 +0200

    Update config.js

--- app/config.js
+++ app/config.js
@@ -1,3 +1,35 @@
-import localConfig from './config.local'
-
-window.mumbleWebConfig = localConfig
+window.mumbleWebConfig = {
+  connectDialog: {
+    address: false,
+    port: false,
+    token: false,
+    username: true,
+    password: true,
+    channelName: false
+  },
+  settings: {
+    voiceMode: 'vad',
+    audioBitrate: 40000,
+    samplesPerPacket: 960
+  },
+  defaults: {
+    address: 'mumble05.omgserv.com',
+    port: '10002',
+    username: '',
+    password: '',
+    theme: '',
+    startMute: false,
+    startDeaf: false,
+    joinDialog: true
+  },
+  hideOverlay: true,
+  hideChannelTree: true,
+  hideSelfDeaf: true,
+  hideSettings: true,
+  hideUsers: true,
+  hideConfigure: true,
+  hideHelp: true,
+  hideAbout: true,
+  hideConnectionStatus: true,
+  hideComment: true
+}
