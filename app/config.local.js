3923778cb6390dafb0f3c4cd31f88f5a17878ac3
Debauns <laurensvanwesting@gmail.com>
2025-04-10 17:11:49 +0200

    Update config.local.js

--- app/config.local.js
+++ app/config.local.js
@@ -2,7 +2,19 @@
 // There should never be any required changes to this file and you can always
 // simply copy it over when updating to a new version.
 
-let config = window.mumbleWebConfig // eslint-disable-line no-unused-vars
+export default {
+  defaultAddress: 'mumble05.omgserv.com',
+  defaultPort: 10002,
+  defaultUsername: '',
+  defaultPassword: '',
+  tokens: [],
+  joinOnConnect: true,
+  hideAddress: true,
+  hidePort: true,
+  hideTokens: true,
+  hidePassword: false,
+  showCertificateWarning: false,
+};
 
 // E.g. changing default address and theme:
 // config.defaults.address = 'voice.example.com'
